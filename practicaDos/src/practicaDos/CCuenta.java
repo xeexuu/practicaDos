cf271d6eba91b4e661a109627e68335e38c63065
mint <mint@mint-VirtualBox>
2021-05-26 09:14:09 +0200

    "Primera versión del proyecto antes de refactorizar".

---
+++ practicaDos/src/practicaDos/CCuenta.java
@@ -0,0 +1,139 @@
+package practicaDos;
+
+
+
+public class CCuenta {
+
+
+
+    private String nombre;
+
+    private String cuenta;
+
+    private double saldo;
+
+    private double tipoInteres;
+
+
+
+    public CCuenta()
+
+    {
+
+    }
+
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+
+    {
+
+        nombre =nom;
+
+        cuenta=cue;
+
+        saldo=sal;
+
+    }
+
+
+    public void asignarNombre(String nom)
+
+    {
+
+        setNombre(nom);
+
+    }
+
+
+    public String obtenerNombre()
+
+    {
+
+        return getNombre();
+
+    }
+
+
+
+    public double estado()
+
+    {
+
+        return saldo;
+
+    }
+
+
+
+    public void ingresar(double cantidad) throws Exception
+
+    {
+
+        if (cantidad<0)
+
+            throw new Exception("No se puede ingresar una cantidad negativa");
+
+        saldo = saldo + cantidad;
+
+    }
+
+
+    public void retirar(double cantidad) throws Exception
+
+    {
+
+        if (cantidad <= 0)
+
+            throw new Exception ("No se puede retirar una cantidad negativa");
+
+        if (estado()< cantidad)
+
+            throw new Exception ("No se hay suficiente saldo");
+
+        saldo = saldo - cantidad;
+
+    }
+
+
+
+    public String obtenerCuenta()
+
+    {
+
+        return cuenta;
+
+    }
+
+
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+
+    }
+
+
+
+    public double getTipoInteres() {
+
+        return tipoInteres;
+
+    }
+
+
+
+    public void setTipoInteres(double tipoInteres) {
+
+        this.tipoInteres = tipoInteres;
+
+    }
+
+}
